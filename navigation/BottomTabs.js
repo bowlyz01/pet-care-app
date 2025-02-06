4697b94d1f6b988d7e7878a072015375fa5ae142
bowlyz <putita.paiboontanasombut@gmail.com>
2025-02-06 20:32:26 +0700

    Add BottomTabs navigation with Home and Calendar screens

---
+++ navigation/BottomTabs.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Entypo, FontAwesome } from '@expo/vector-icons';
+import HomeScreen from '../screens/HomeScreen';
+import AddPetScreen from '../screens/AddPetScreen';
+import CalendarScreen from '../screens/CalendarScreen';
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'home';
+            return <Entypo name={iconName} size={size} color={color} />;
+          } else if (route.name === 'Calendar') {
+            iconName = 'calendar';
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          }
+        },
+        tabBarActiveTintColor: '#45BCD8',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: { backgroundColor: 'white', paddingBottom: 5, height: 60 },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="Calendar" component={CalendarScreen} options={{ headerShown: false }} />
+    </Tab.Navigator>
+  );
+}
