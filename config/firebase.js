bec3c4bdf4520254374580f986752722e2b78e21
bowlyz <putita.paiboontanasombut@gmail.com>
2025-02-08 07:37:10 +0700

    Add Firestore initialization to Firebase config; export Firestore instance

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-
-import {getReactNativePersistence, initializeAuth} from 'firebase/auth';
+import { getFirestore } from "firebase/firestore"; // Import Firestore
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // add your app on firebase, copy firebaseConfig here, enable email/password auth
@@ -17,6 +17,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Initialize Firestore
+export const db = getFirestore(app); // Export Firestore instance
+
+// Initialize Auth
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
+  persistence: getReactNativePersistence(AsyncStorage),
 });
