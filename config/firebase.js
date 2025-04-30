4f68306b96dea9fb1a9818e53fca6cc3b3cfaae6
putita_pai <157984891+bowlyz01@users.noreply.github.com>
2025-04-30 16:56:38 +0700

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -5,13 +5,13 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // add your app on firebase, copy firebaseConfig here, enable email/password auth
 const firebaseConfig = {
-  apiKey: "AIzaSyCkaDLf-eiYrSLNoHY-f9Y__bUZCT9w0FI",
-  authDomain: "pet-care-expo.firebaseapp.com",
-  projectId: "pet-care-expo",
-  storageBucket: "pet-care-expo.firebasestorage.app",
-  messagingSenderId: "850728381712",
-  appId: "1:850728381712:web:d2a473d7e33fd4d6d5c995",
-  measurementId: "G-G4YVWW06ED"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
